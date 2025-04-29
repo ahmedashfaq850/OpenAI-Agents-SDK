b7cc0bb37f6361cccf2dd534557a7244be261346
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2025-04-29 14:40:20 +0800

    add packages

---
+++ README.md
@@ -0,0 +1,2 @@
+1. setup uv package manager
+2. install libraries using uv (openai-agents, python-dotenv)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from open-ai-agents-sdk!")
-
-
-if __name__ == "__main__":
-    main()
